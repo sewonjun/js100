eb43a3d27307e080695ca3d9794da7ce61d56764
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-07-06 11:19:24 +0900

    6,7,8

---
+++ 8.js
@@ -0,0 +1,12 @@
+//자바스크립트 객체를 다음과 같이 만들었다. 출력값을 입력하시오. 
+
+var d = {
+    height : 180,
+    weight : 78,
+    weight : 84, 
+    temperature : 36,
+    eyesight : 1 
+};
+
+console.log(d.weight);
+console.log['eyesight']
