3527f0c21b41172fd70da5ab1c8a2382126b40b4
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-07-29 23:53:12 +0900

    43 try

---
+++ 43.js
@@ -0,0 +1,10 @@
+//사용자에게 숫자를 입력받고 이를 2진수로 바꾸고 그 값을 출력해주세요.
+
+const var1 = valueOf(){return prompt("insert a number");
+
+let var2 = parseInt(var1, 2);
+
+let val3 = parseInt(var2, 2);
+
+//parseInt에 대해 다시 공부하기 
+//2진법 공부하기 
