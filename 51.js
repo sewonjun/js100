e66ec44ecff2d6968249437c60a783f128014d0a
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-08-09 14:00:40 +0900

    재귀함수 이해못함

--- 51.js
+++ 51.js
@@ -2,6 +2,7 @@
 
 //코드의 반킨을 채워 병합정렬을 완성하시오.
 
+//나누는 부분
 function mergeSort(arr) {
   if (arr.length <= 1) {
     return arr;
@@ -14,23 +15,26 @@ function mergeSort(arr) {
   return merge(mergeSort(left), mergeSort(right));
 }
 
+//정렬하고 병합하는 부분
 function merge(left, right) {
   let result = [];
-  while (/*빈칸 채우세용*/ && /*빈칸 채우세용*/){
-    if (/*빈칸 채우세용*/){
-        result.push(left.shift());
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
     } else {
-        result.push(right.shift());
+      result.push(right.shift());
     }
   }
-  while(left.length){
-    /*빈칸 채우세용*/
+  while (left.length) {
+    result.push(left.shift());
   }
-  while(right.length){
-    /*빈칸 채우세용*/ 
+  while (right.length) {
+    result.push(right.shift());
   }
   return result;
 }
 
-const array = prompt('배열을 입력하세요').split('').map(n => parseInt(n,10));
+const array = prompt("배열을 입력하세요")
+  .split("")
+  .map((n) => parseInt(n, 10));
 console.log(mergeSort(array));
