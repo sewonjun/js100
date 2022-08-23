4b25942e9130f9e867ca9b756cd0c39f1c6b46aa
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-08-23 16:51:33 +0900

    comment

--- 19.js
+++ 19.js
@@ -1,13 +1,12 @@
 //공백으로 구분하여 두  숫자 a와 b가 주어지면, a의 b승을 구하는 프로그램을 작성하세요 ~~
 
-
 //내 답
-var math =prompt("insert something");
+var math = prompt("insert something");
 
-var mathSplit = math.split('');
-console.log(mathSplit);
+var mathSplit = math.split("");
+// console.log(mathSplit);
 
-console.log(mathSplit[0]);
+// console.log(mathSplit[0]);
 
 var multiple = mathSplit[0] ** mathSplit[1];
 
@@ -16,6 +15,8 @@ console.log(multiple);
 //모범답
 const num = prompt().split("");
 
-console.log(Math.pow(parseInt(num[0], 10), parseInt(num[1],10)));
+console.log(Math.pow(parseInt(num[0], 10), parseInt(num[1], 10)));
 
-//Math.pow => 제곱 시켜주는 함수. 첫번째 매개변수가 base가 되고 두번째를 exponent로 받아들여 제곱한다. 
+//Math.pow => 제곱 시켜주는 함수. 첫번째 매개변수가 base가 되고 두번째를 exponent로 받아들여 제곱한다.
+
+// 항상 정수가 나와야 하는 식은 parseInt 써주자.
