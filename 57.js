49c761a33d8bf39d1581c07b7b9c906c50461672
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-08-30 00:40:44 +0900

    57

---
+++ 57.js
@@ -0,0 +1,13 @@
+let array = [];
+for (i = 0; i <= 1000; i++) {
+  array.push(i);
+}
+
+// console.log(array);
+
+let a = array.join(" ");
+// console.log(a);
+let b = array.join("");
+// console.log(b);
+let c = array.split("");
+console.log(c);
