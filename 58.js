f3f49a2f3e098b33e2ff283b5685cdcd2fd6167a
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-08-31 14:41:10 +0900

    58

--- 58.js
+++ 58.js
@@ -1,4 +1,18 @@
 //숫자를 입력 받고 천단위로 콤마를 찍어주시오.
-
-const n = prompt("숫자를 입력하세요");
+const n = prompt("insert number");
 parseInt(n, 10);
+
+const divideN = n / 3;
+const remainderN = n % 3;
+if (remainderN !== 0) {
+  let a = n.slice(remainderN) + ",";
+  console.log(a);
+}
+
+for (let i = 1; i < divideN; i++) {
+  if (i === 1) {
+    let a = n.slice(remainderN, remainderN + 3) + ",";
+    let b = n.slice(remainderN + 3, remainder + 6);
+    return a;
+  }
+}
