108fe5821659079e27f1c138ff772d560be35811
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-09-11 00:13:25 +0900

    67 final

--- 67.js
+++ 67.js
@@ -17,17 +17,25 @@ const 악수의수 = 59;
 console.log(solution(악수의수));
 
 //내 풀이
-let 총악수 = parseInt(54, 10);
+let 총악수 = parseInt(45, 10);
 let result = [];
 for (i = 1; i++; ) {
-  let 악수 = (i * i - 1) / 2;
+  let 악수 = (i * (i - 1)) / 2;
+  console.log("악수", 악수);
+  if (총악수 === 악수) {
+    result = i - 1;
+    break;
+  }
   if (총악수 < 악수) {
+    // result = i;
     break;
   } else {
     result = i;
   }
 }
 
+console.log("result", result);
 let 사람의수 = result;
+console.log("민규를 뺀 악수 수", (사람의수 * (사람의수 - 1)) / 2);
 let 민규악수 = 총악수 - (사람의수 * (사람의수 - 1)) / 2;
 console.log(민규악수, 사람의수 + 1);
