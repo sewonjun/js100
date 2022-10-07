ac6d464cf524713fc77d8b7e9dbeab4d1b8a74e3
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-10-07 10:53:14 +0900

    91  fin

--- 91.js
+++ 91.js
@@ -1,7 +1,5 @@
 let student_score = [];
-
 let class_score = [];
-
 let total_score = [];
 
 for (let k = 0; k < 7; k++) {
@@ -17,25 +15,30 @@ for (let k = 0; k < 7; k++) {
   total_score.push(class_score);
 }
 
-// console.log(total_score);
-
 let class_average = [];
 for (let c of total_score) {
   let class_sum = 0;
-  //console.log(c); // 각 반의 학생들 점수 리스트
-  let total_sum = [];
+  console.log(`각반의 학생들 점수 리스트${c}`); // 각 반의 학생들 점수 리스트
+  let individual_average = []; //개개인의 평균
   for (s of c) {
     let sum = 0;
     for (let i = 0; i < s.length; i++) {
       sum += s[i];
     }
-    //console.log(`각 학생의 평균${sum}`); // 각 학생의 평균
-    total_sum.push(Math.floor(sum / 5));
+    individual_average.push(Math.floor(sum / 5));
   }
-  console.log(total_sum); // 각 학생들의 평균 리스트
-  for (q of total_sum) {
+  console.log(`각 학생 개개인의 평균 리스트${individual_average}`); // 각 학생들의 평균 리스트
+  console.log(`반 1등의 점수 ${Math.max(...individual_average)}`);
+  for (q of individual_average) {
     class_sum += q;
   }
   class_average.push(Math.floor(class_sum / c.length));
 }
-console.log(` 각반의 평균 리스트${class_average}`); // 반평균 리스트
+console.log(`각반의 평균 리스트${class_average}`); // 반평균 리스트
+
+let total_average = 0; //전체평균
+for (a of class_average) {
+  total_average += a;
+}
+
+console.log(`전교 평균${Math.floor(total_average / 7)}`);
