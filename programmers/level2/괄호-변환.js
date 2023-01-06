4b948157813acb68396702253b5fd53e337f6c15
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-01-06 17:28:16 +0900

    괄호-변환

--- programmers/level2/괄호-변환.js
+++ programmers/level2/괄호-변환.js
@@ -1,41 +1,74 @@
-// () {} []
-//왼쪽으로 이동시켰을 때 올바른 괄호가 되는 x의 갯수;
-const s = "[](){}";
-const small = ["(", ")"];
-const middle = ["{", "}"];
-const large = ["[", "]"];
-function solution() {
-  var answer = -1;
-
-  return answer;
-}
+// const s = "[](){}";
+const s = "}]()[{";
 const bracket = {
   "(": ")",
   "{": "}",
   "[": "]",
 };
-console.log(bracket["("]);
-const splitedS = s.split("");
-console.log(splitedS);
-const falseBox = [];
-// splitedS.forEach((element) => {
-//   if (bracket[element] === undefined) {
-//     falseBox.push(false);
-//   } else {
-//     const rightBracket = bracket[element];
-//     const rightIdx = splitedS.indexOf(rightBracket);
-//     splitedS[rightIdx] = "";
-//     console.log(splitedS);
-//   }
-// });
-for (let i = 0; i < splitedS.length; i++) {
-  const element = splitedS[i];
-  if (bracket[element] === undefined) {
+const leftBracket = Object.keys(bracket);
+const rightBracket = Object.values(bracket);
+function solution(s) {
+  var answer = 0;
+  for (let i = 0; i < s.length; i++) {
+    let splitedS = s.split("");
+    slicedS = splitedS.splice(0, i);
+    splitedS = splitedS.concat(slicedS);
+    validateBracket(splitedS) === false ? answer : (answer += 1);
+  }
+  return answer;
+}
+function validateBracket(arrayBracket) {
+  if (countBracket(arrayBracket) === false) {
+    return false;
+  } else if (orderBracket(arrayBracket) === false) {
     return false;
   } else {
-    const rightBracket = bracket[element];
-    const rightIdx = splitedS.indexOf(rightBracket);
-    splitedS[rightIdx] = "";
-    console.log(splitedS);
+    for (let i = 0; i < arrayBracket.length; i++) {
+      const element = arrayBracket[i];
+      if (element === "") {
+        continue;
+      } else if (bracket[element] === undefined) {
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
 }
+let answer = validateBracket(["(", "[", "{", ")", "}", "]"]);
+//갯수로만 알려주는 validation
+function countBracket(arrayBracket) {
+  let leftCount = 0;
+  let rightCount = 0;
+
+  arrayBracket.forEach((element) => {
+    if (leftBracket.includes(element)) {
+      leftCount += 1;
+    }
+    if (rightBracket.includes(element)) {
+      rightCount += 1;
+    }
+  });
+  if (leftCount === rightCount) {
+    return true;
+  } else {
+    return false;
+  }
+}
+//순서가 옳은지 알려주는 validation
+function orderBracket(arrayBracket) {
+  let temp = [];
+  for (let i = 0; i < arrayBracket.length; i++) {
+    let element = arrayBracket[i];
+    if (leftBracket.includes(element)) {
+      temp.push(element);
+    } else {
+      let lastElement = temp.pop();
+      if (element !== bracket[lastElement]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+console.log(solution(s));
